2f0e9a06a2c8e7c6470057b4743ae5a828a5073f
Si Young Sung <ssung13@umd.edu>
2022-04-24 14:59:28 -0400

    fixed minor mistakes

--- app/src/main/java/com/example/classlib/CheckoutActivity.kt
+++ app/src/main/java/com/example/classlib/CheckoutActivity.kt
@@ -8,6 +8,12 @@ import android.widget.EditText
 import android.widget.Spinner
 
 class CheckoutActivity : AppCompatActivity() {
+    private lateinit var bookTitle: EditText
+    private lateinit var studentID: EditText
+    private lateinit var barcodeButton: Button
+    private lateinit var returnButton: Button
+    private lateinit var checkoutButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
@@ -16,7 +22,11 @@ class CheckoutActivity : AppCompatActivity() {
 
     }
     private fun initializeUI() {
-
+        bookTitle = findViewById(R.id.book_title_return)
+        studentID = findViewById(R.id.student_id_return)
+        barcodeButton = findViewById(R.id.scan_barcode_return)
+        checkoutButton = findViewById(R.id.checkout_book_Button)
+        returnButton = findViewById(R.id.return_book)
     }
 
 }
