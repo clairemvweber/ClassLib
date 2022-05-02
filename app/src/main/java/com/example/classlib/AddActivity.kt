2c3387d5f51deeaf019dd77e86cc3e798818bf25
Ian Sebin <iserbin01@gmail.com>
2022-05-02 13:14:05 -0400

    Added isbn lookup functionality

--- app/src/main/java/com/example/classlib/AddActivity.kt
+++ app/src/main/java/com/example/classlib/AddActivity.kt
@@ -1,13 +1,11 @@
 package com.example.classlib
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -21,14 +19,16 @@ class AddActivity : AppCompatActivity() {
     private lateinit var bookCopies: EditText
     private lateinit var scanBtn: Button
     private lateinit var addBtn: Button
+    private lateinit var categories: Array<String>
     var userEmail = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
         userEmail = intent.getStringExtra("username").toString()
-        //Log.d("ClassLib", userEmail)
+        categories = resources.getStringArray(R.array.Categories)
         initializeUI()
         addBtn.setOnClickListener { addBook() }
+        scanBtn.setOnClickListener{ isbnLookup()}
 
     }
 
@@ -66,13 +66,58 @@ class AddActivity : AppCompatActivity() {
         bookLexile = findViewById(R.id.book_lexile)
         bookAge = findViewById(R.id.book_age)
         bookCategory = findViewById(R.id.category_spinner)
+        if (bookCategory!= null){
+            val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, categories)
+            bookCategory.adapter=adapter
+        }
         bookCopies = findViewById(R.id.book_copies)
-        scanBtn = findViewById(R.id.scan_button)
+        scanBtn = findViewById(R.id.search_isbn_button)
         addBtn = findViewById(R.id.add_button)
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.top_menu, menu)
         return true
+    }
+    private fun isbnLookup() {
+
+        startActivityForResult(
+            Intent(
+                this@AddActivity,
+                LookupActivity::class.java
+            ),0
+        )
+    }
+    private fun clear(){
+        bookTitle.setText(null)
+        bookAuthor.setText(null)
+        bookLexile.setText(null)
+        bookCategory.setSelection(0)
+        bookAge.setText(null)
+        bookCopies.setText(null)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data != null) {
+            clear()
+            bookTitle.setText(data.getStringExtra(TITLE_TAG))
+            bookAuthor.setText(data.getStringExtra(AUTHORS_TAG))
+            bookLexile.setText(data.getStringExtra(LEXILE_TAG))
+            bookCategory.setSelection(categories.indexOf(data.getStringExtra(CATEGORIES_TAG)))
+            if (data.getStringExtra(MIN_AGE_TAG) != "null"){
+                bookAge.setText(data.getStringExtra(MIN_AGE_TAG))
+            }
+        }
+    }
+    companion object {
+
+        const val ISBN_TAG = "canonical_isbn"
+        const val TITLE_TAG = "title"
+        const val AUTHORS_TAG = "authors"
+        const val CATEGORIES_TAG = "categories"
+        const val LEXILE_TAG = "lexile"
+        const val MIN_AGE_TAG = "min_age"
+        const val MAX_AGE_TAG = "max_age"
+
     }
 }
